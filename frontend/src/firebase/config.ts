501d3aa0eed652a4947e5dc83823935724f1475c
Eduardo P de Sousa <edupsousa@gmail.com>
2022-08-20 14:05:47 -0300

    inicializado app firebase

---
+++ frontend/src/firebase/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBoy7-RjfC5zqxbcqGpYXPZx9ttYr797vI",
+  authDomain: "js-dojo-33166.firebaseapp.com",
+  databaseURL: "https://js-dojo-33166-default-rtdb.firebaseio.com",
+  projectId: "js-dojo-33166",
+  storageBucket: "js-dojo-33166.appspot.com",
+  messagingSenderId: "80893925243",
+  appId: "1:80893925243:web:dfc91d9df52a62480ecfde",
+  measurementId: "G-KVPB05Q52Z",
+};
