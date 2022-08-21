601b6ce70fae84b7dd62210fecdee9dc0ddc4992
Eduardo P de Sousa <edupsousa@gmail.com>
2022-08-21 16:52:12 -0300

    layout das abas no componente DojoUtils

--- frontend/src/components/DojoChat.tsx
+++ frontend/src/components/DojoChat.tsx
@@ -1,11 +1,74 @@
 function DojoChat() {
   return (
     <div className="dojoChat">
-      <h1>Dojo Chat</h1>
-      <p>
-        This is a dojo chat application. It is a proof of concept for a dojo
-        chat application.
-      </p>
+      <div className="header">
+        <h2>Mensagens</h2>
+        <form>
+          <input type="text" placeholder="Digite sua mensagem" />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+      <ul className="messages">
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          harum mollitia quos provident aspernatur consequuntur nam cumque earum
+          vitae nobis modi deleniti unde repellendus voluptatum non, aliquid
+          perspiciatis, laborum nihil?
+        </li>
+        <li>
+          Dolorum itaque esse, voluptatum facere nostrum error molestiae modi
+          nihil aliquam ipsa vero veniam corporis est autem accusamus eveniet
+          incidunt porro omnis nam a. Doloremque quia eligendi cupiditate
+          veritatis quos?
+        </li>
+        <li>
+          Nisi a esse fugit tempora maiores ipsa laudantium molestias sit vero
+          facilis eveniet amet vel officia iure, quo, suscipit dolores officiis.
+          Aliquid dolor, similique non dolorum odio quibusdam. Aliquid, at!
+        </li>
+        <li>
+          Nesciunt eaque ea consectetur eius quis dolore similique quae,
+          praesentium quod mollitia! Iusto, expedita repellendus quod aut quia
+          adipisci. Ab in ipsam, inventore nihil et accusantium? Officia
+          temporibus pariatur ratione.
+        </li>
+        <li>
+          Eum fugit, nobis nisi ipsum consequuntur excepturi deserunt earum
+          adipisci quam, repellat ex unde fuga eaque, at alias? Reiciendis ab
+          corrupti voluptatum accusantium excepturi rem maxime omnis culpa, et
+          quia.
+        </li>
+        <li>
+          Maiores ipsum numquam quaerat fugit perspiciatis. Perspiciatis porro
+          animi harum consequuntur quidem sed dolores, est nobis beatae,
+          nesciunt sunt eius fugiat voluptatum accusantium numquam quam? Alias
+          dolore ipsa aut corrupti.
+        </li>
+        <li>
+          Natus exercitationem eum ratione facilis accusantium saepe, libero
+          voluptatibus reiciendis enim suscipit delectus unde cumque atque
+          facere mollitia illo vero deserunt voluptates? Cumque dolore omnis
+          perspiciatis minus molestiae, fugiat ipsum!
+        </li>
+        <li>
+          Eum maxime temporibus itaque velit modi fugit cumque fuga est
+          provident voluptatibus qui sunt enim, tempore pariatur illo expedita
+          architecto vero possimus, eaque quod deserunt consectetur consequatur.
+          Repudiandae, fugit architecto!
+        </li>
+        <li>
+          Impedit commodi recusandae voluptates. Voluptates neque obcaecati
+          adipisci assumenda eius pariatur incidunt nisi ex voluptatem amet
+          doloribus veniam, quod magni nostrum ut deserunt itaque ducimus quia
+          aliquam voluptas mollitia a!
+        </li>
+        <li>
+          Soluta possimus praesentium rerum perspiciatis obcaecati sunt sint
+          exercitationem, tenetur at doloribus hic accusantium similique
+          mollitia, officiis nemo deleniti, ducimus libero sit maiores illo
+          tempore inventore saepe! Dolorum, animi veniam.
+        </li>
+      </ul>
     </div>
   );
 }
--- frontend/src/components/DojoUtils.tsx
+++ frontend/src/components/DojoUtils.tsx
@@ -1,7 +1,17 @@
 function DojoUtils() {
   return (
     <div className="dojoUtils">
-      <h1>DojoUtils</h1>
+      <ul className="tabs">
+        <li className="active">
+          <button>Desafio</button>
+        </li>
+        <li>
+          <button>Console</button>
+        </li>
+        <li>
+          <button>Participantes</button>
+        </li>
+      </ul>
     </div>
   );
 }
