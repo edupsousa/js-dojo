453e7558f40be76b60f43953f0835b9db99c347d
Eduardo P de Sousa <edupsousa@gmail.com>
2022-08-21 17:12:40 -0300

    layout do componente DojoChat

--- frontend/src/components/DojoChat.tsx
+++ frontend/src/components/DojoChat.tsx
@@ -10,63 +10,94 @@ function DojoChat() {
       </div>
       <ul className="messages">
         <li>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          harum mollitia quos provident aspernatur consequuntur nam cumque earum
-          vitae nobis modi deleniti unde repellendus voluptatum non, aliquid
-          perspiciatis, laborum nihil?
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            harum mollitia quos provident aspernatur consequuntur nam cumque
+            earum vitae nobis modi deleniti unde repellendus voluptatum non,
+            aliquid perspiciatis, laborum nihil?
+          </span>
         </li>
         <li>
-          Dolorum itaque esse, voluptatum facere nostrum error molestiae modi
-          nihil aliquam ipsa vero veniam corporis est autem accusamus eveniet
-          incidunt porro omnis nam a. Doloremque quia eligendi cupiditate
-          veritatis quos?
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Dolorum itaque esse, voluptatum facere nostrum error molestiae modi
+            nihil aliquam ipsa vero veniam corporis est autem accusamus eveniet
+            incidunt porro omnis nam a. Doloremque quia eligendi cupiditate
+            veritatis quos?
+          </span>
         </li>
         <li>
-          Nisi a esse fugit tempora maiores ipsa laudantium molestias sit vero
-          facilis eveniet amet vel officia iure, quo, suscipit dolores officiis.
-          Aliquid dolor, similique non dolorum odio quibusdam. Aliquid, at!
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Nisi a esse fugit tempora maiores ipsa laudantium molestias sit vero
+            facilis eveniet amet vel officia iure, quo, suscipit dolores
+            officiis. Aliquid dolor, similique non dolorum odio quibusdam.
+            Aliquid, at!
+          </span>
         </li>
         <li>
-          Nesciunt eaque ea consectetur eius quis dolore similique quae,
-          praesentium quod mollitia! Iusto, expedita repellendus quod aut quia
-          adipisci. Ab in ipsam, inventore nihil et accusantium? Officia
-          temporibus pariatur ratione.
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Nesciunt eaque ea consectetur eius quis dolore similique quae,
+            praesentium quod mollitia! Iusto, expedita repellendus quod aut quia
+            adipisci. Ab in ipsam, inventore nihil et accusantium? Officia
+            temporibus pariatur ratione.
+          </span>
         </li>
         <li>
-          Eum fugit, nobis nisi ipsum consequuntur excepturi deserunt earum
-          adipisci quam, repellat ex unde fuga eaque, at alias? Reiciendis ab
-          corrupti voluptatum accusantium excepturi rem maxime omnis culpa, et
-          quia.
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Eum fugit, nobis nisi ipsum consequuntur excepturi deserunt earum
+            adipisci quam, repellat ex unde fuga eaque, at alias? Reiciendis ab
+            corrupti voluptatum accusantium excepturi rem maxime omnis culpa, et
+            quia.
+          </span>
         </li>
         <li>
-          Maiores ipsum numquam quaerat fugit perspiciatis. Perspiciatis porro
-          animi harum consequuntur quidem sed dolores, est nobis beatae,
-          nesciunt sunt eius fugiat voluptatum accusantium numquam quam? Alias
-          dolore ipsa aut corrupti.
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Maiores ipsum numquam quaerat fugit perspiciatis. Perspiciatis porro
+            animi harum consequuntur quidem sed dolores, est nobis beatae,
+            nesciunt sunt eius fugiat voluptatum accusantium numquam quam? Alias
+            dolore ipsa aut corrupti.
+          </span>
         </li>
         <li>
-          Natus exercitationem eum ratione facilis accusantium saepe, libero
-          voluptatibus reiciendis enim suscipit delectus unde cumque atque
-          facere mollitia illo vero deserunt voluptates? Cumque dolore omnis
-          perspiciatis minus molestiae, fugiat ipsum!
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Natus exercitationem eum ratione facilis accusantium saepe, libero
+            voluptatibus reiciendis enim suscipit delectus unde cumque atque
+            facere mollitia illo vero deserunt voluptates? Cumque dolore omnis
+            perspiciatis minus molestiae, fugiat ipsum!
+          </span>
         </li>
         <li>
-          Eum maxime temporibus itaque velit modi fugit cumque fuga est
-          provident voluptatibus qui sunt enim, tempore pariatur illo expedita
-          architecto vero possimus, eaque quod deserunt consectetur consequatur.
-          Repudiandae, fugit architecto!
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Eum maxime temporibus itaque velit modi fugit cumque fuga est
+            provident voluptatibus qui sunt enim, tempore pariatur illo expedita
+            architecto vero possimus, eaque quod deserunt consectetur
+            consequatur. Repudiandae, fugit architecto!
+          </span>
         </li>
         <li>
-          Impedit commodi recusandae voluptates. Voluptates neque obcaecati
-          adipisci assumenda eius pariatur incidunt nisi ex voluptatem amet
-          doloribus veniam, quod magni nostrum ut deserunt itaque ducimus quia
-          aliquam voluptas mollitia a!
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Impedit commodi recusandae voluptates. Voluptates neque obcaecati
+            adipisci assumenda eius pariatur incidunt nisi ex voluptatem amet
+            doloribus veniam, quod magni nostrum ut deserunt itaque ducimus quia
+            aliquam voluptas mollitia a!
+          </span>
         </li>
         <li>
-          Soluta possimus praesentium rerum perspiciatis obcaecati sunt sint
-          exercitationem, tenetur at doloribus hic accusantium similique
-          mollitia, officiis nemo deleniti, ducimus libero sit maiores illo
-          tempore inventore saepe! Dolorum, animi veniam.
+          <span className="from">Eduardo</span>
+          <span className="message">
+            Soluta possimus praesentium rerum perspiciatis obcaecati sunt sint
+            exercitationem, tenetur at doloribus hic accusantium similique
+            mollitia, officiis nemo deleniti, ducimus libero sit maiores illo
+            tempore inventore saepe! Dolorum, animi veniam.
+          </span>
         </li>
       </ul>
     </div>
